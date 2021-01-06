f429f2d0a921cf0b9f6b47170ec59bca5d18c7d3
DebugST <2212233137@qq.com>
2021-01-06 15:52:14 +0800

    add license to code

--- ST.Library.UI/STNodeEditor/STNode.cs
+++ ST.Library.UI/STNodeEditor/STNode.cs
@@ -6,7 +6,35 @@ using System.Text;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Collections;
+/*
+MIT License
 
+Copyright (c) 2021 DebugST@crystal_lz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+/*
+ * time: 2021-01-06
+ * Author: Crystal_lz
+ * blog: st233.com
+ * Github: DebugST.github.io
+ */
 namespace ST.Library.UI
 {
     public abstract class STNode
